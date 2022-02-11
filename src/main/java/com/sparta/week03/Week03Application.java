09e539a1d53cd13d969c1b29fe006244155da800
jeonghyeon <skjh0807s@snu.ac.kr>
2022-02-12 01:25:01 +0900

    게시글 week3 기능까지, 댓글들 보기 위한 버튼 추가

--- src/main/java/com/sparta/week03/Week03Application.java
+++ src/main/java/com/sparta/week03/Week03Application.java
@@ -2,7 +2,10 @@ package com.sparta.week03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaAuditing // 꼭 넣어주어야 해요:)
 @SpringBootApplication
 public class Week03Application {
 
