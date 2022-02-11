aef57f86f850e5846968cac2512bd722f04b419f
jeonghyeon <skjh0807s@snu.ac.kr>
2022-02-11 20:10:08 +0900

    Initial commit

---
+++ src/main/java/com/sparta/week03/domain/MemoRequestDto.java
@@ -0,0 +1,12 @@
+package com.sparta.week03.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RequiredArgsConstructor
+@Getter
+public class MemoRequestDto {
+    private final String username;
+    private final String contents;
+}
