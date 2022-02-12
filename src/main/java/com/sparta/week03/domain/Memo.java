15e2f01c8999a5d5c38ca181787c7d00f2e44632
jeonghyeon <skjh0807s@snu.ac.kr>
2022-02-13 03:27:20 +0900

    RDS연결

--- src/main/java/com/sparta/week03/domain/Memo.java
+++ src/main/java/com/sparta/week03/domain/Memo.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @Entity // 테이블과 연계됨을 스프링에게 알려줍니다.
 public class Memo extends Timestamped { // 생성,수정 시간을 자동으로 만들어줍니다.
 
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
 
